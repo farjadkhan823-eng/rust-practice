0846b24eafceb69a5963f07663c0d35104fcf301
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-07-30 21:30:25 +0500

    module three

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod module_one;
+mod module_two;
+mod module_three;
+
 fn main() {
-    // println!("Hello world from main function");
     module_one::module_one_func();
+    module_two::module_two_func();
+    module_three::ownership_rules();
 }
---
+++ src/module_two.rs
@@ -0,0 +1,9 @@
+pub fn module_two_func() {
+    // let first_name: String = "Farjad".to_string();
+    // let last_name: String = "Khan".to_string();
+    // println!("{}", outer_func(first_name, last_name));
+}
+
+// fn outer_func(first_name: String, last_name: String) -> String {
+//     return first_name + " " + &last_name;
+// }
