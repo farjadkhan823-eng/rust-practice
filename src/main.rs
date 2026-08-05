9c33cff22187f83b8bd8832940fefd9d9f37ecdd
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-08-05 21:17:51 +0500

    avoiding ownership

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+mod avoiding_ownership;
 mod module_one;
-mod module_two;
 mod module_three;
-
+mod module_two;
 fn main() {
     module_one::module_one_func();
     module_two::module_two_func();
     module_three::ownership_rules();
+    avoiding_ownership::avoid_owner();
 }
--- src/module_three.rs
+++ src/module_three.rs
@@ -1,17 +1,17 @@
-const GLOBAL: u8 = 100;
+// const GLOBAL: u8 = 100;
 
 pub fn ownership_rules() {
-    let num1 = 10;
-    {
-        let num2 = 20;
-        println!("from inner scope {}", num2);
-        println!("from result {}", num1 + num2);
-    }
+    // let num1 = 10;
+    // {
+    //     let num2 = 20;
+    //     println!("from inner scope {}", num2);
+    //     println!("from result {}", num1 + num2);
+    // }
 
-    println!("num1 from outer scope {}", num1);
-    global_func();
+    // println!("num1 from outer scope {}", num1);
+    // global_func();
 }
 
-fn global_func() {
-    print!("globally value from global func {}", GLOBAL);
-}
+// fn global_func() {
+//     print!("globally value from global func {}", GLOBAL);
+// }
