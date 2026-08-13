bec394508ea9c96386e7d5862bfaf5919eefc86d
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-08-13 22:02:39 +0500

    borowing duplicate without double or more variables

--- src/task_four.rs
+++ src/task_four.rs
@@ -1,9 +1,9 @@
 pub fn borrowing() {
-    let city: String = String::from("Karachi");
-    let copy_city: String = city_func(&city);
-    println!("original {} and copy {}", city, copy_city);
+    let original_city: String = String::from("Karachi");
+    city_func(&original_city);
+    println!("original {}", original_city);
 }
 
-fn city_func(city: &String) -> String {
-    return city.to_string();
+fn city_func(duplicate_city: &String) {
+    println!("duplicate {}", duplicate_city);
 }
