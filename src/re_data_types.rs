173c0f20ccfadaac334edc2a39edab75b537c253
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-08-14 21:04:21 +0500

    dt_struct_ex

--- src/re_data_types.rs
+++ src/re_data_types.rs
@@ -1,3 +1,11 @@
+#[derive(Debug)]
+struct StudentData {
+    name: String,
+    program: String,
+    semester: u32,
+    result: f32,
+}
+
 pub fn data_types() {
     // // ARRAY //
     // let week: [&str; 7] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -17,16 +25,51 @@ pub fn data_types() {
     // marks.pop();
     // println!("marks: {:?}", marks);
 
-    // TUPPLE //
-    let employee: (&str, i32, i32) = ("Farjad", 20, 85000);
+    // // TUPPLE //
+    // let employee: (&str, i32, i32) = ("Farjad", 20, 85000);
 
-    println!("name {}", employee.0);
-    println!("age {}", employee.1);
-    println!("salary {}", employee.2);
+    // println!("name {}", employee.0);
+    // println!("age {}", employee.1);
+    // println!("salary {}", employee.2);
 
-    let (name, age, salary) = employee;
+    // let (name, age, salary) = employee;
 
-    println!("name {}", name);
-    println!("age {}", age);
-    println!("salary {}", salary);
+    // println!("name {}", name);
+    // println!("age {}", age);
+    // println!("salary {}", salary);
+
+    // STRUCT //
+
+    // let std_one = StudentData {
+    //     name: String::from("Samad"),
+    //     class: String::from("6th"),
+    //     marks: 490,
+    // };
+
+    // println!("{:#?}", std_one);
+
+    let mut student_list: Vec<StudentData> = Vec::new();
+
+    student_list.push(StudentData {
+        name: String::from("Samad"),
+        program: String::from("BS-Software Engineering"),
+        semester: 5,
+        result: 2.90,
+    });
+
+    student_list.push(StudentData {
+        name: String::from("Farjad"),
+        program: String::from("BS-Software Engineering"),
+        semester: 3,
+        result: 3.80,
+    });
+
+    student_list.push(StudentData {
+        name: String::from("Arman"),
+        program: String::from("BS-Computer Science"),
+        semester: 4,
+        result: 3.65,
+    });
+
+    println!("{:#?}", student_list);
 }
